ee9bb42aed8e07f1e3b0b1bc7f57c5ec0e4de639
Tron <leon.seyfert@hotmail.com>
2025-11-21 17:11:50 +0100

    feat: added Statemachine
    added statemachine foundation for walking and idle

---
+++ scripts/player/statemachine/state_machine.gd
@@ -0,0 +1,36 @@
+class_name StateMachine
+
+extends Node
+
+@export var current_state: State
+var states: Dictionary = {}
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name] = child
+			child.transition.connect(on_child_transition)
+		else:
+			push_warning("State machine contrains incompatible child node")
+	
+	current_state.enter()
+
+
+func _process(delta):
+	current_state.update(delta)
+	Global.debug.add_property("Current State",current_state.name,1)
+
+
+func _physics_process(delta: float):
+	current_state.physics_update(delta)
+
+
+func on_child_transition(new_state_name: StringName) -> void:
+	var new_state = states.get(new_state_name)
+	if new_state != null:
+		if new_state != current_state:
+			current_state.exit()
+			new_state.enter()
+			current_state = new_state
+		else:
+			push_warning("State does not exist")
