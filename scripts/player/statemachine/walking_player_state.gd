9367546c70f890720df05d227def5c902907b526
Tron <leon.seyfert@hotmail.com>
2025-12-01 14:33:14 +0100

    fix: you can now only sprint forwards

--- scripts/player/statemachine/walking_player_state.gd
+++ scripts/player/statemachine/walking_player_state.gd
@@ -20,5 +20,5 @@ func handle_input(event: InputEvent):
 	if event.is_action_pressed("crouch") or event.is_action_pressed("crouch_toggle"):
 		transition.emit("CrouchingPlayerState")
 		
-	if event.is_action_pressed("sprint") and player.is_on_floor():
+	if Input.is_action_pressed("walk_forwards") and Input.is_action_pressed("sprint") and player.is_on_floor():
 		transition.emit("SprintingPlayerState")
